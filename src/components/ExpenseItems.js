81adce210806823ab1a941700d3c1365f580d066
Rahul <amitroe1999@gmail.com>
2023-04-02 18:23:02 +0000

    AExpenseItems compoent added

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,16 @@
+function ExpenseItems() {
+    return (
+        <div>
+            <h1>Expense Items!!</h1>
+            <div>
+                <h2>March 28th 2023</h2>
+                <div>
+                    <h2>Car Insurance</h2>
+                    <h3>$700</h3>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItems;
