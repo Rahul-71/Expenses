ab3e6ef932300e73f388c0467c5ae899923783f8
Rahul <amitroe1999@gmail.com>
2023-04-03 15:58:05 +0000

    Deleting expense on a click of a button

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -4,11 +4,21 @@ import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItems.css";
 
 function ExpenseItems(props) {
+    const clickHandler = () => {
+        console.log("clicked !!");
+    }
+
+    const deleteExpense = (event) => {
+        const row = event.target.parentNode;
+        row.remove();
+    }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <ExpenseDetails title={props.title} amount={props.amount} location={props.location} />
+            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={deleteExpense}>Delete Expense</button>
         </Card >
     );
 }
